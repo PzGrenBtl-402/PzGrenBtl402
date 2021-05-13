74353905ff12dbf5cbbd4c3a104307d75625d252
BemonLarries <matti31798@gmail.com>
2021-05-14 00:31:25 +0200

    Add ACE compatibility for GM MG3

---
+++ addons/PzGrenBtl402_GM_Machineguns/CfgMagazines.hpp
@@ -0,0 +1,8 @@
+class CfgMagazines
+{
+  class gm_magazine_762x51mm_belt_base;
+  class gm_120rnd_762x51mm_mg3_base: gm_magazine_762x51mm_belt_base
+  {
+    ACE_isBelt = 1;
+  };
+};
---
+++ addons/PzGrenBtl402_GM_Machineguns/CfgWeapons.hpp
@@ -0,0 +1,9 @@
+class CfgWeapons
+{
+	class gm_machineGun_base;
+	class gm_mg3_base: gm_machineGun_base
+	{
+		ace_overheating_allowSwapBarrel = 1;
+		ace_overheating_dispersion = 1;
+	};
+};
