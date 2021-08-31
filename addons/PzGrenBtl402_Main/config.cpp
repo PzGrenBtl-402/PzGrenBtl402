1e077483e025ae0dc05cbc2f7a7648eb8c2ffd44
Zumiii <nilsgraf@yahoo.de>
2021-08-31 22:10:24 +0200

    Add Main Addon

---
+++ addons/PzGrenBtl402_Main/config.cpp
@@ -0,0 +1,34 @@
+
+#include "script_component.hpp"
+
+class CfgPatches {
+  class ADDON {
+    name = COMPONENT_NAME;
+    units[] = {};
+    weapons[] = {};
+    requiredVersion = REQUIRED_VERSION;
+    requiredAddons[] = {"cba_main","ace_main"};
+    author = "Zumi";
+    url = "https://pzgrenbtl402.de";
+    VERSION_CONFIG;
+  };
+};
+
+class CfgMods {
+    class PREFIX {
+        dir = "@PzGrenBtl402";
+        name = "PzGrenBtl 402";
+        picture = "z\PzGrenBtl402\PzGrenBtl402_Logo.paa";
+        hidePicture = 0;
+        hideName = 0;
+        actionName = "Website";
+        action = "https://pzgrenbtl402.de";
+        description = "Go to Website...";
+    };
+};
+
+
+#include "cfgSettings.hpp"
+#include "cfgEventHandlers.hpp"
+#include "cfgWeapons.hpp"
+#include "cfgVehicles.hpp"
---
+++ addons/PzGrenBtl402_Main/script_component.hpp
@@ -0,0 +1,18 @@
+#define COMPONENT main
+#define COMPONENT_BEAUTIFIED Main
+
+//#include "script_mod.hpp"
+
+#include "\z\PzGrenBtl402\addons\main\script_mod.hpp"
+
+#ifdef DEBUG_ENABLED_MAIN
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_MAIN
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_MAIN
+#endif
+
+
+#include "\z\PzGrenBtl402\addons\main\script_macros.hpp"
+//#include "script_macros.hpp"
