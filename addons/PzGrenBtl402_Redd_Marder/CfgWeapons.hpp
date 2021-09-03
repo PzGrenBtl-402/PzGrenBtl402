8c930dab264ff3a25d796f74ae63c06fc1eb9eea
Timi007 <timi007gp@gmail.com>
2021-09-04 01:52:17 +0200

    Remove tab

--- addons/PzGrenBtl402_Redd_Marder/CfgWeapons.hpp
+++ addons/PzGrenBtl402_Redd_Marder/CfgWeapons.hpp
@@ -2,7 +2,7 @@ class CfgWeapons {
     class Redd_SmokeLauncher;
     class GVAR(SmokeLauncher): Redd_SmokeLauncher {
         magazines[] = {"Redd_SmokeLauncherMag"};
-		showToPlayer = 0;
+        showToPlayer = 0;
         magazineReloadTime = 5;
     };
 };
