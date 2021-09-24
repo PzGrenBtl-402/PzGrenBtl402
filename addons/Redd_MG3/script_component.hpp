7b12afb3fd3053e4c854e8d2871f49cb68af454c
Timi007 <timi007gp@gmail.com>
2021-09-25 01:20:47 +0200

    Enable cache

--- addons/Redd_MG3/script_component.hpp
+++ addons/Redd_MG3/script_component.hpp
@@ -3,7 +3,7 @@
 #include "\z\PzGrenBtl402\addons\Main\script_mod.hpp"
 
 // #define DEBUG_MODE_FULL
-#define DISABLE_COMPILE_CACHE
+// #define DISABLE_COMPILE_CACHE
 // #define CBA_DEBUG_SYNCHRONOUS
 
 #ifdef DEBUG_ENABLED_REDD_MG3
