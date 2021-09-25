ef1325c133cdce91b0ec9406cd544f272b14f7ad
Timi007 <timi007gp@gmail.com>
2021-09-25 19:10:53 +0200

    Untabify

--- addons/Redd_NiArms_Sounds/CfgWeapons.hpp
+++ addons/Redd_NiArms_Sounds/CfgWeapons.hpp
@@ -8,16 +8,16 @@ class CfgWeapons {
 
         class FullAuto: Mode_FullAuto {
             class BaseSoundModeType
-			{
-				weaponSoundEffect = "DefaultRifle";
-				closure1[] = {"\hlc_wp_Mg3\snd\mg3_first",1,1,10};
-				closure2[] = {"\hlc_wp_Mg3\snd\mg3_first",1,1,10};
-				soundClosure[] = {"closure1",0.5,"closure2",0.5};
-			};
-			class StandardSound: BaseSoundModeType
-			{
-				soundSetShot[] = {"NIA_MG3_NEW_Shot_SoundSet","MG3_Tail_SoundSet"};
-			};
+            {
+                weaponSoundEffect = "DefaultRifle";
+                closure1[] = {"\hlc_wp_Mg3\snd\mg3_first",1,1,10};
+                closure2[] = {"\hlc_wp_Mg3\snd\mg3_first",1,1,10};
+                soundClosure[] = {"closure1",0.5,"closure2",0.5};
+            };
+            class StandardSound: BaseSoundModeType
+            {
+                soundSetShot[] = {"NIA_MG3_NEW_Shot_SoundSet","MG3_Tail_SoundSet"};
+            };
         };
     };
 };
