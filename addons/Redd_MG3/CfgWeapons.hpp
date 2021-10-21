3c635c06dc25e7374ecdee282a791598db70125c
Timi007 <timi007gp@gmail.com>
2021-10-21 21:22:13 +0200

    Use same ACE overheating values like GM compat

--- addons/Redd_MG3/CfgWeapons.hpp
+++ addons/Redd_MG3/CfgWeapons.hpp
@@ -3,9 +3,12 @@ class CfgWeapons {
     class Redd_MG3_Static: Redd_MG3 {
         magazineReloadTime = 0.1;
         autoReload = 1;
+
+        // Same values as in ACE MG3 GM compat
+        ace_overheating_mrbs = 3000;
+        ace_overheating_slowdownFactor = 1;
         ace_overheating_allowSwapBarrel = 1;
-        ace_overheating_dispersion = 0.75;
-        ace_overheating_mrbs = 2000;
+        ace_overheating_dispersion = 0.25;
 
         magazineWell[] = {GVAR(mg3_762x51)};
         magazines[] += { // ACE CSW needs all compatible magazines in this array for reloading
