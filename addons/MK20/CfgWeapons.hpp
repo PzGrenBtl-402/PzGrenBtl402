0e855a0d30b71f1c1462b26baafa632bee031174
Timi007 <timi007gp@gmail.com>
2022-02-20 19:22:04 +0100

    Make item only accessible via script

--- addons/MK20/CfgWeapons.hpp
+++ addons/MK20/CfgWeapons.hpp
@@ -12,14 +12,14 @@ class CfgWeapons {
     };
 
     class GVAR(he_ammo): GVAR(ammo_base) {
-        scope = 2;
+        scope = 1;
         displayName = CSTRING(heAmmoDisplayName);
         descriptionShort = CSTRING(heAmmoDescription);
         descriptionUse = CSTRING(heAmmoDescription);
         picture = QPATHTOF(data\ui\he_ammo_item_icon.paa);
     };
     class GVAR(ap_ammo): GVAR(ammo_base) {
-        scope = 2;
+        scope = 1;
         displayName = CSTRING(apAmmoDisplayName);
         descriptionShort = CSTRING(apAmmoDescription);
         descriptionUse = CSTRING(apAmmoDescription);
