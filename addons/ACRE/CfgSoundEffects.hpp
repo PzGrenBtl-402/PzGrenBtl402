35394a95acae8266e53d57cb3577181dfb34d5d3
Timi007 <timi007gp@gmail.com>
2022-03-27 18:52:59 +0200

    Allow players in and turned out to hear each other
    Allow players inside and turned out to hear each other but not from differen compartments (future ACRE feature)

--- addons/ACRE/CfgSoundEffects.hpp
+++ addons/ACRE/CfgSoundEffects.hpp
@@ -4,10 +4,13 @@ class CfgSoundEffects
     {
         class TankAttenuation
         {
-            // Set to 2 to disable any attenuation between players which are turned out and turned in.
+            // WHEN USING ACRE 2.9.1.1047: Set to 2 to disable any attenuation between players which are turned out and turned in.
             // Normally 0.5 of this value is applied. (0 = No attenuation, 1 = Max attenuation)
             // https://github.com/IDI-Systems/acre2/blob/master/addons/sys_attenuate/fnc_getUnitAttenuate.sqf#L51
-            acreAttenuation = 2;
+            // acreAttenuation = 2;
+
+            acreAttenuation = 1;
+            acreAttenuationTurnedOut = 0.5;
         };
     };
 };
