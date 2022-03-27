6a981b826137453ab505c9c89e0afb3edecd54d3
mishkar89 <mishkar89@gmail.com>
2022-03-27 09:14:42 +0200

    Adjust GM Fuchs intercom

--- addons/GM_Fuchs/CfgVehicles.hpp
+++ addons/GM_Fuchs/CfgVehicles.hpp
@@ -1,14 +1,19 @@
-class CfgVehicles {
+class CfgVehicles
+{
     class gm_wheeled_APC_base;
-    class gm_fuchs_base: gm_wheeled_APC_base {
+    class gm_fuchs_base: gm_wheeled_APC_base
+    {
         class UserActions;
     };
 
-    class gm_fuchsa0_base: gm_fuchs_base {
+    class gm_fuchsa0_base: gm_fuchs_base
+    {
         maximumLoad = 10000;
 
-        class UserActions: UserActions {
-            class hecktueren_auf {
+        class UserActions: UserActions
+        {
+            class hecktueren_auf
+            {
                 displayName = CSTRING(openDoors);
                 position = "hatch_1_1_axis";
                 radius = 2;
@@ -18,7 +23,8 @@ class CfgVehicles {
                 statement = "this animateDoor ['door_2_2_source', 1, false]; this animateDoor ['door_2_1_source', 1, false];";
             };
 
-            class hecktueren_zu {
+            class hecktueren_zu
+            {
                 displayName = CSTRING(closeDoors);
                 position = "hatch_1_1_axis";
                 radius = 2;
@@ -28,7 +34,8 @@ class CfgVehicles {
                 statement = "this animateDoor ['door_2_1_source', 0, false]; this animateDoor ['door_2_2_source', 0, false];";
             };
 
-            class hecktueren_auf_2 {
+            class hecktueren_auf_2
+            {
                 displayName = CSTRING(openDoors);
                 position = "actionPoint";
                 radius = 10;
@@ -38,7 +45,8 @@ class CfgVehicles {
                 statement = "this animateDoor ['door_2_2_source', 1, false]; this animateDoor ['door_2_1_source', 1, false];";
             };
 
-            class hecktueren_zu_2 {
+            class hecktueren_zu_2
+            {
                 displayName = CSTRING(closeDoors);
                 position = "actionPoint";
                 radius = 10;
@@ -50,15 +58,18 @@ class CfgVehicles {
         };
     };
 
-    class gm_fuchsa0_engineer_base: gm_fuchsa0_base {
-        class AcreIntercoms {
-            class Intercom_1 {
+    class gm_fuchsa0_engineer_base: gm_fuchsa0_base
+    {
+        class AcreIntercoms
+        {
+            class Intercom_1
+            {
                 displayName = ECSTRING(ACRE, BV);
                 shortName = ECSTRING(ACRE, BVShort);
-                allowedPositions[] = {"driver", "commander", {"turret", {2}}};
+                allowedPositions[] = {"driver", {"turret", {0}, {2}}};
                 disabledPositions[] = {};
-                limitedPositions[] = {"gunner", {"cargo", "all"}, {"turret", {3}, {4}, {5}}};
-                numLimitedPositions = 2;
+                limitedPositions[] = {{"cargo", "all"}, {"turret", {1}, {3}, {4}, {5}}};
+                numLimitedPositions = 3;
                 masterPositions[] = {};
                 connectedByDefault = 1;
             };
@@ -72,8 +83,10 @@ class CfgVehicles {
         acre_eventInfantryPhone = QEFUNC(ACRE, noApiFunction);
         acre_infantryPhonePosition[] = {-1.025, -3.4, -0.75};
 
-        class AcreRacks {
-            class Rack_1 {
+        class AcreRacks
+        {
+            class Rack_1
+            {
                 displayName = ECSTRING(ACRE, RackA);
                 shortName = ECSTRING(ACRE, RackAShort);
                 componentName = "ACRE_SEM90";
@@ -87,15 +100,18 @@ class CfgVehicles {
         };
     };
 
-    class gm_fuchsa0_reconnaissance_base: gm_fuchsa0_base {
-        class AcreIntercoms {
-            class Intercom_1 {
+    class gm_fuchsa0_reconnaissance_base: gm_fuchsa0_base
+    {
+        class AcreIntercoms
+        {
+            class Intercom_1
+            {
                 displayName = ECSTRING(ACRE, BV);
                 shortName = ECSTRING(ACRE, BVShort);
-                allowedPositions[] = {"driver", "commander", {"turret", {4}}};
+                allowedPositions[] = {"driver", {"turret", {0}, {4}}};
                 disabledPositions[] = {};
-                limitedPositions[] = {"gunner", {"cargo", "all"}, {"turret", {2}, {3}, {5}}};
-                numLimitedPositions = 2;
+                limitedPositions[] = {{"cargo", "all"}, {"turret", {1}, {2}, {3}, {5}}};
+                numLimitedPositions = 3;
                 masterPositions[] = {};
                 connectedByDefault = 1;
             };
@@ -109,8 +125,10 @@ class CfgVehicles {
         acre_eventInfantryPhone = QEFUNC(ACRE, noApiFunction);
         acre_infantryPhonePosition[] = {-1.025, -3.4, -0.75};
 
-        class AcreRacks {
-            class Rack_1 {
+        class AcreRacks
+        {
+            class Rack_1
+            {
                 displayName = ECSTRING(ACRE, RackA);
                 shortName = ECSTRING(ACRE, RackAShort);
                 componentName = "ACRE_SEM90";
@@ -122,7 +140,8 @@ class CfgVehicles {
                 intercom[] = {"all"};
             };
 
-            class Rack_2 {
+            class Rack_2
+            {
                 displayName = ECSTRING(ACRE, RackB);
                 shortName = ECSTRING(ACRE, RackBShort);
                 componentName = "ACRE_SEM90";
@@ -136,15 +155,18 @@ class CfgVehicles {
         };
     };
 
-    class gm_fuchsa0_command_base: gm_fuchsa0_base {
-        class AcreIntercoms {
-            class Intercom_1 {
+    class gm_fuchsa0_command_base: gm_fuchsa0_base
+    {
+        class AcreIntercoms
+        {
+            class Intercom_1
+            {
                 displayName = ECSTRING(ACRE, BV);
                 shortName = ECSTRING(ACRE, BVShort);
-                allowedPositions[] = {"driver", "commander", {"turret", {1}}};
+                allowedPositions[] = {"driver", {"turret", {0}, {1}}};
                 disabledPositions[] = {};
-                limitedPositions[] = {{"cargo", "all"}, {"turret", {2}}, {"turret", {3}}};
-                numLimitedPositions = 2;
+                limitedPositions[] = {{"cargo", "all"}, {"turret", {2}, {3}}};
+                numLimitedPositions = 3;
                 masterPositions[] = {};
                 connectedByDefault = 1;
             };
@@ -158,8 +180,10 @@ class CfgVehicles {
         acre_eventInfantryPhone = QEFUNC(ACRE, noApiFunction);
         acre_infantryPhonePosition[] = {-1.025, -3.4, -0.75};
 
-        class AcreRacks {
-            class Rack_1 {
+        class AcreRacks
+        {
+            class Rack_1
+            {
                 displayName = ECSTRING(ACRE, RackA);
                 shortName = ECSTRING(ACRE, RackAShort);
                 componentName = "ACRE_SEM90";
@@ -171,7 +195,8 @@ class CfgVehicles {
                 intercom[] = {"all"};
             };
 
-            class Rack_2 {
+            class Rack_2
+            {
                 displayName = ECSTRING(ACRE, RackB);
                 shortName = ECSTRING(ACRE, RackBShort);
                 componentName = "ACRE_SEM90";
@@ -183,7 +208,8 @@ class CfgVehicles {
                 intercom[] = {"all"};
             };
 
-            class Rack_3 {
+            class Rack_3
+            {
                 displayName = ECSTRING(ACRE, RackC);
                 shortName = ECSTRING(ACRE, RackCShort);
                 componentName = "ACRE_SEM90";
