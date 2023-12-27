c16978110d169ddadb6f4d3afd49395fd891db1c
Timi007 <timi007gp@gmail.com>
2023-12-27 23:05:13 +0100

    Rename addon

--- addons/GM_Structures/script_component.hpp
+++ addons/GM_Structures/script_component.hpp
@@ -1,17 +1,17 @@
-#define COMPONENT GM_Structure
-#define COMPONENT_BEAUTIFIED GM Structure
+#define COMPONENT GM_Structures
+#define COMPONENT_BEAUTIFIED GM Structures
 #include "\z\PzGrenBtl402\addons\Main\script_mod.hpp"
 
 // #define DEBUG_MODE_FULL
 // #define DISABLE_COMPILE_CACHE
 // #define CBA_DEBUG_SYNCHRONOUS
 
-#ifdef DEBUG_ENABLED_GM_STRUCTURE
+#ifdef DEBUG_ENABLED_GM_STRUCTURES
     #define DEBUG_MODE_FULL
 #endif
 
-#ifdef DEBUG_SETTINGS_GM_STRUCTURE
-    #define DEBUG_SETTINGS DEBUG_SETTINGS_GM_STRUCTURE
+#ifdef DEBUG_SETTINGS_GM_STRUCTURES
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_GM_STRUCTURES
 #endif
 
 #include "\z\PzGrenBtl402\addons\Main\script_macros.hpp"
