4366aa0cddf76ab05284a929ebe64a31ff93efb9
Timi007 <timi007gp@gmail.com>
2023-12-28 00:50:43 +0100

    Set high armor only for building where it makes sense

--- addons/GM_Structures/CfgVehicles.hpp
+++ addons/GM_Structures/CfgVehicles.hpp
@@ -8,7 +8,147 @@ class CfgVehicles {
         };
     };
 
-    class gm_structure_euro_80_base: gm_structure_euro_base {
-        armor = 100000;
-    };
+    #define SET_ARMOR(BASE,CLS) class CLS: BASE { armor = 100000; }
+
+    class gm_structure_euro_80_commercial_01d_base;
+    SET_ARMOR(gm_structure_euro_80_commercial_01d_base,land_gm_euro_church_01);
+    SET_ARMOR(gm_structure_euro_80_commercial_01d_base,land_gm_euro_church_02);
+    SET_ARMOR(gm_structure_euro_80_commercial_01d_base,land_gm_euro_pub_01);
+    SET_ARMOR(gm_structure_euro_80_commercial_01d_base,land_gm_euro_pub_01_win);
+
+    class gm_structure_euro_80_commercial_02d_base;
+    SET_ARMOR(gm_structure_euro_80_commercial_02d_base,land_gm_euro_office_02);
+    SET_ARMOR(gm_structure_euro_80_commercial_02d_base,land_gm_euro_office_02_win);
+    SET_ARMOR(gm_structure_euro_80_commercial_02d_base,land_gm_euro_office_03);
+    SET_ARMOR(gm_structure_euro_80_commercial_02d_base,land_gm_euro_office_03_win);
+    SET_ARMOR(gm_structure_euro_80_commercial_02d_base,land_gm_euro_pub_02);
+    SET_ARMOR(gm_structure_euro_80_commercial_02d_base,land_gm_euro_pub_02_win);
+    SET_ARMOR(gm_structure_euro_80_commercial_02d_base,land_gm_euro_shop_01_w);
+    SET_ARMOR(gm_structure_euro_80_commercial_02d_base,land_gm_euro_shop_01_w_win);
+    SET_ARMOR(gm_structure_euro_80_commercial_02d_base,land_gm_euro_shop_02_e);
+    SET_ARMOR(gm_structure_euro_80_commercial_02d_base,land_gm_euro_shop_02_e_win);
+    SET_ARMOR(gm_structure_euro_80_commercial_02d_base,land_gm_euro_shop_02_w);
+    SET_ARMOR(gm_structure_euro_80_commercial_02d_base,land_gm_euro_shop_02_w_win);
+
+    class gm_structure_euro_80_commercial_03d_base;
+    SET_ARMOR(gm_structure_euro_80_commercial_03d_base,land_gm_euro_fuelstation_01_w);
+    SET_ARMOR(gm_structure_euro_80_commercial_03d_base,land_gm_euro_fuelstation_01_w_win);
+
+    class gm_structure_euro_80_commercial_04d_base;
+    SET_ARMOR(gm_structure_euro_80_commercial_04d_base,land_gm_euro_mine_01);
+
+    class gm_structure_euro_80_commercial_15d_base;
+    SET_ARMOR(gm_structure_euro_80_commercial_15d_base,land_gm_euro_office_01);
+    SET_ARMOR(gm_structure_euro_80_commercial_15d_base,land_gm_euro_office_01_win);
+
+    class gm_structure_euro_80_house_01d_base;
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_01_d);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_01_d_win);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_01_e);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_01_e_win);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_01_w);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_01_w_win);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_02_d);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_02_d_win);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_02_e);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_02_e_win);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_02_w);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_02_w_win);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_03_e);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_03_e_win);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_03_w);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_03_w_win);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_05_d);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_05_d_win);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_05_e);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_05_e_win);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_05_w);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_05_w_win);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_06_d);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_06_d_win);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_06_e);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_06_e_win);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_06_w);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_06_w_win);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_07_d);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_07_d_win);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_07_e);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_07_e_win);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_07_w);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_07_w_win);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_08_d);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_08_d_win);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_08_e);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_08_e_win);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_08_w);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_08_w_win);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_09_d);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_09_d_win);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_09_e);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_09_e_win);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_09_w);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_09_w_win);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_10_d);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_10_d_win);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_10_e);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_10_e_win);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_10_w);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_10_w_win);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_11_d);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_11_d_win);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_11_e);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_11_e_win);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_11_w);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_11_w_win);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_12_d);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_12_d_win);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_12_e);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_12_e_win);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_12_w);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_12_w_win);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_13_d);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_13_d_win);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_13_e);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_13_e_win);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_13_w);
+    SET_ARMOR(gm_structure_euro_80_house_01d_base,land_gm_euro_house_13_w_win);
+
+    class gm_structure_euro_80_house_05d_base;
+    SET_ARMOR(gm_structure_euro_80_house_05d_base,land_gm_euro_house_04_d);
+    SET_ARMOR(gm_structure_euro_80_house_05d_base,land_gm_euro_house_04_d_win);
+    SET_ARMOR(gm_structure_euro_80_house_05d_base,land_gm_euro_house_04_e);
+    SET_ARMOR(gm_structure_euro_80_house_05d_base,land_gm_euro_house_04_e_win);
+    SET_ARMOR(gm_structure_euro_80_house_05d_base,land_gm_euro_house_04_w);
+    SET_ARMOR(gm_structure_euro_80_house_05d_base,land_gm_euro_house_04_w_win);
+
+    class gm_structure_euro_80_infrastructure_00d_base;
+    SET_ARMOR(gm_structure_euro_80_infrastructure_00d_base,land_gm_euro_farmhouse_01);
+    SET_ARMOR(gm_structure_euro_80_infrastructure_00d_base,land_gm_euro_farmhouse_01_win);
+    SET_ARMOR(gm_structure_euro_80_infrastructure_00d_base,land_gm_euro_farmhouse_02);
+    SET_ARMOR(gm_structure_euro_80_infrastructure_00d_base,land_gm_euro_farmhouse_02_win);
+
+    class gm_structure_euro_80_infrastructure_01d_base;
+    SET_ARMOR(gm_structure_euro_80_infrastructure_01d_base,land_gm_euro_farmhouse_03);
+    SET_ARMOR(gm_structure_euro_80_infrastructure_01d_base,land_gm_euro_farmhouse_03_win);
+    SET_ARMOR(gm_structure_euro_80_infrastructure_01d_base,land_gm_euro_shed_01);
+    SET_ARMOR(gm_structure_euro_80_infrastructure_01d_base,land_gm_euro_shed_01_win);
+    SET_ARMOR(gm_structure_euro_80_infrastructure_01d_base,land_gm_tower_bt_6_fuest_80);
+
+    class gm_structure_euro_80_infrastructure_02d_base;
+    SET_ARMOR(gm_structure_euro_80_infrastructure_02d_base,land_gm_euro_barracks_01);
+    SET_ARMOR(gm_structure_euro_80_infrastructure_02d_base,land_gm_euro_barracks_01_win);
+    SET_ARMOR(gm_structure_euro_80_infrastructure_02d_base,land_gm_euro_barracks_02);
+    SET_ARMOR(gm_structure_euro_80_infrastructure_02d_base,land_gm_euro_barracks_02_win);
+    SET_ARMOR(gm_structure_euro_80_infrastructure_02d_base,land_gm_euro_factory_02);
+    SET_ARMOR(gm_structure_euro_80_infrastructure_02d_base,land_gm_euro_factory_02_win);
+
+    class gm_structure_euro_80_infrastructure_05d_base;
+    SET_ARMOR(gm_structure_euro_80_infrastructure_05d_base,land_gm_euro_shed_05);
+    SET_ARMOR(gm_structure_euro_80_infrastructure_05d_base,land_gm_euro_shed_05_win);
+
+    class gm_structure_euro_80_infrastructure_13d_base;
+    SET_ARMOR(gm_structure_euro_80_infrastructure_13d_base,land_gm_euro_factory_01_01);
+    SET_ARMOR(gm_structure_euro_80_infrastructure_13d_base,land_gm_euro_factory_01_01_win);
+    SET_ARMOR(gm_structure_euro_80_infrastructure_13d_base,land_gm_euro_shed_04);
+    SET_ARMOR(gm_structure_euro_80_infrastructure_13d_base,land_gm_euro_shed_04_win);
 };
