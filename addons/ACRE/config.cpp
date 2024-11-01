94af09ef5b336d52aeea9d9940973a6f9d32f821
Timi007 <timi007gp@gmail.com>
2024-11-01 16:41:27 +0100

    Fix ACRE dependencies

--- addons/ACRE/config.cpp
+++ addons/ACRE/config.cpp
@@ -6,7 +6,12 @@ class CfgPatches {
         units[] = {};
         weapons[] = {};
         requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = {"PzGrenBtl402_Main", "acre_sys_attenuate"};
+        requiredAddons[] = {
+            "PzGrenBtl402_Main",
+            "acre_sys_attenuate",
+            "acre_sys_intercom",
+            "acre_sys_rack"
+        };
         author = ECSTRING(Main,authors);
         authors[] = {"Timi007", "Mishkar"};
         url = ECSTRING(Main,URL);
