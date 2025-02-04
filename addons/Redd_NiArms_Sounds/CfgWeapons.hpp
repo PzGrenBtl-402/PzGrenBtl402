d9f2fb2c4e5c8a71ca8c221dd2c8496219824c52
Timi007 <timi007gp@gmail.com>
2025-02-04 20:26:13 +0100

    Cleanup

--- addons/Redd_NiArms_Sounds/CfgWeapons.hpp
+++ addons/Redd_NiArms_Sounds/CfgWeapons.hpp
@@ -3,16 +3,16 @@ class Mode_FullAuto;
 class CfgWeapons {
     class LMG_coax;
     class Redd_MG3: LMG_coax {
-        reloadmagazinesound[] = {"\hlc_wp_mg3\snd\mg3_reload",0.8,1,18};
-        drysound[] = {"\hlc_wp_mg3\snd\mg3_dryfire",1,1,10};
+        reloadmagazinesound[] = {"\hlc_wp_mg3\snd\mg3_reload.wss", 0.8, 1, 18};
+        drysound[] = {"\hlc_wp_mg3\snd\mg3_dryfire.wss", 1, 1, 10};
 
         class FullAuto: Mode_FullAuto {
             class StandardSound {
                 weaponSoundEffect = "DefaultRifle";
-                closure1[] = {"\hlc_wp_Mg3\snd\mg3_first",1,1,10};
-                closure2[] = {"\hlc_wp_Mg3\snd\mg3_first",1,1,10};
-                soundClosure[] = {"closure1",0.5,"closure2",0.5};
-                soundSetShot[] = {"NIA_MG3_NEW_Shot_SoundSet","MG3_Tail_SoundSet"};
+                closure1[] = {"\hlc_wp_Mg3\snd\mg3_first.wss", 1, 1, 10};
+                closure2[] = {"\hlc_wp_Mg3\snd\mg3_first.wss", 1, 1, 10};
+                soundClosure[] = {"closure1", 0.5, "closure2", 0.5};
+                soundSetShot[] = {"NIA_MG3_NEW_Shot_SoundSet", "MG3_Tail_SoundSet"};
             };
         };
     };
