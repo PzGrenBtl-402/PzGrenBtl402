8503d942d6624a53caf3e907ce2122ba516b28b9
Timi007 <timi007gp@gmail.com>
2025-06-07 14:27:15 +0200

    Add ACRE racks and intercom to GM Gepard

---
+++ addons/GM_Gepard/config.cpp
@@ -0,0 +1,18 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        name = COMPONENT_NAME;
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {"PzGrenBtl402_Main", "PzGrenBtl402_ACRE", "gm_vehicles_land_tracked_gepard"};
+        skipWhenMissingDependencies = 1;
+        author = ECSTRING(Main,authors);
+        authors[] = {"Timi007"};
+        url = ECSTRING(Main,URL);
+        VERSION_CONFIG;
+    };
+};
+
+#include "CfgVehicles.hpp"
---
+++ addons/GM_Gepard/script_component.hpp
@@ -0,0 +1,17 @@
+#define COMPONENT GM_Gepard
+#define COMPONENT_BEAUTIFIED GM Gepard
+#include "\z\PzGrenBtl402\addons\Main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define CBA_DEBUG_SYNCHRONOUS
+
+#ifdef DEBUG_ENABLED_GM_GEPARD
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_GM_GEPARD
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_GM_GEPARD
+#endif
+
+#include "\z\PzGrenBtl402\addons\Main\script_macros.hpp"
