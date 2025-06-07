b8e2ee7b5eed432661d10f391e7c8dd1464e2398
Timi007 <timi007gp@gmail.com>
2025-06-07 14:26:41 +0200

    Add ACRE racks and intercom to GM RakJPz

---
+++ addons/GM_RakJPz/CfgVehicles.hpp
@@ -0,0 +1,60 @@
+class CfgVehicles
+{
+    class gm_tracked_Tank_base;
+    class gm_rakjpz2_base: gm_tracked_Tank_base
+    {
+        maximumLoad = 10000;
+
+        class AcreIntercoms
+        {
+            class Intercom_1
+            {
+                displayName = ECSTRING(ACRE,BV);
+                shortName = ECSTRING(ACRE,BVShort);
+                allowedPositions[] = {"crew"};
+                disabledPositions[] = {};
+                limitedPositions[] = {};
+                numLimitedPositions = 0;
+                masterPositions[] = {};
+                connectedByDefault = 1;
+            };
+        };
+
+        acre_hasInfantryPhone = 0;
+        acre_infantryPhoneDisableRinging = 1;
+        acre_infantryPhoneCustomRinging[] = {};
+        acre_infantryPhoneIntercom[] = {"all"};
+        acre_infantryPhoneControlActions[] = {"all"};
+        acre_eventInfantryPhone = QEFUNC(ACRE,noApiFunction);
+        acre_infantryPhonePosition[] = {};
+
+        class AcreRacks
+        {
+            class Rack_1
+            {
+                displayName = ECSTRING(ACRE,RackA);
+                shortName = ECSTRING(ACRE,RackAShort);
+                componentName = "ACRE_SEM90";
+                allowedPositions[] = {"crew"};
+                disabledPositions[] = {};
+                defaultComponents[] = {};
+                mountedRadio = "ACRE_SEM70";
+                isRadioRemovable = 0;
+                intercom[] = {"all"};
+            };
+
+            class Rack_2
+            {
+                displayName = ECSTRING(ACRE,RackB);
+                shortName = ECSTRING(ACRE,RackBShort);
+                componentName = "ACRE_SEM90";
+                allowedPositions[] = {"crew"};
+                disabledPositions[] = {};
+                defaultComponents[] = {};
+                mountedRadio = "ACRE_SEM70";
+                isRadioRemovable = 0;
+                intercom[] = {"all"};
+            };
+        };
+    };
+};
---
+++ addons/GM_RakJPz/script_component.hpp
@@ -0,0 +1,17 @@
+#define COMPONENT GM_RakJPz
+#define COMPONENT_BEAUTIFIED GM RakJPz
+#include "\z\PzGrenBtl402\addons\Main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define CBA_DEBUG_SYNCHRONOUS
+
+#ifdef DEBUG_ENABLED_GM_RAKJPZ
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_GM_RAKJPZ
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_GM_RAKJPZ
+#endif
+
+#include "\z\PzGrenBtl402\addons\Main\script_macros.hpp"
