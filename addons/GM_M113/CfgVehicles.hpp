10eb62b868b75b1334ca961033f92d62e20d6989
Timi007 <timi007gp@gmail.com>
2025-06-07 14:47:51 +0200

    Add ramp action to M113 and ACRE config for command and mortar

--- addons/GM_M113/CfgVehicles.hpp
+++ addons/GM_M113/CfgVehicles.hpp
@@ -1,6 +1,10 @@
 class CfgVehicles
 {
-    class gm_tracked_APC_base;
+    class gm_tracked_base;
+    class gm_tracked_APC_base: gm_tracked_base {
+        class UserActions;
+    };
+
     class gm_m113_base: gm_tracked_APC_base
     {
         maximumLoad = 10000;
@@ -47,6 +51,96 @@ class CfgVehicles
             {
                 displayName = ECSTRING(ACRE,RackB);
                 shortName = ECSTRING(ACRE,RackBShort);
+                componentName = "ACRE_SEM90";
+                allowedPositions[] = {"crew"};
+                disabledPositions[] = {};
+                defaultComponents[] = {};
+                mountedRadio = "ACRE_SEM70";
+                isRadioRemovable = 0;
+                intercom[] = {"all"};
+            };
+        };
+
+        class UserActions: UserActions
+        {
+            class ramp_open
+            {
+                displayName = CSTRING(openRamp);
+                position = "actionPoint";
+                radius = 2;
+                onlyforplayer = 1;
+                showWindow = 0;
+                condition = "(alive this) && {this doorPhase 'hatch_2_1_source' isEqualTo 0} && {(call CBA_fnc_currentUnit) in this}";
+                statement = "this animateDoor ['hatch_2_1_source', 1, false];";
+            };
+
+            class ramp_close
+            {
+                displayName = CSTRING(closeRamp);
+                position = "actionPoint";
+                radius = 2;
+                onlyforplayer = 1;
+                showWindow = 0;
+                condition = "(alive this) && {this doorPhase 'hatch_2_1_source' isEqualTo 1} && {(call CBA_fnc_currentUnit) in this}";
+                statement = "this animateDoor ['hatch_2_1_source', 0, false];";
+            };
+        };
+    };
+
+    class gm_m113a1g_base;
+    class gm_m113a1g_command_base: gm_m113a1g_base
+    {
+        class AcreRacks
+        {
+            class Rack_1
+            {
+                displayName = ECSTRING(ACRE,RackA);
+                shortName = ECSTRING(ACRE,RackAShort);
+                componentName = "ACRE_SEM90";
+                allowedPositions[] = {"crew"};
+                disabledPositions[] = {};
+                defaultComponents[] = {};
+                mountedRadio = "ACRE_SEM70";
+                isRadioRemovable = 0;
+                intercom[] = {"all"};
+            };
+
+            class Rack_2
+            {
+                displayName = ECSTRING(ACRE,RackB);
+                shortName = ECSTRING(ACRE,RackBShort);
+                componentName = "ACRE_SEM90";
+                allowedPositions[] = {"crew"};
+                disabledPositions[] = {};
+                defaultComponents[] = {};
+                mountedRadio = "ACRE_SEM70";
+                isRadioRemovable = 0;
+                intercom[] = {"all"};
+            };
+
+            class Rack_3
+            {
+                displayName = ECSTRING(ACRE,RackC);
+                shortName = ECSTRING(ACRE,RackCShort);
+                componentName = "ACRE_SEM90";
+                allowedPositions[] = {"crew"};
+                disabledPositions[] = {};
+                defaultComponents[] = {};
+                mountedRadio = "ACRE_SEM70";
+                isRadioRemovable = 0;
+                intercom[] = {"all"};
+            };
+        };
+    };
+
+    class gm_m113a1g_mortar_base: gm_m113a1g_base
+    {
+        class AcreRacks
+        {
+            class Rack_1
+            {
+                displayName = ECSTRING(ACRE,RackA);
+                shortName = ECSTRING(ACRE,RackAShort);
                 componentName = "ACRE_SEM90";
                 allowedPositions[] = {"crew"};
                 disabledPositions[] = {};
