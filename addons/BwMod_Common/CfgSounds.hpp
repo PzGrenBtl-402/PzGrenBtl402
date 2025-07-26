ab03c7d3327a3b8d27e0360f68a8a89317887e99
Timi007 <timi007gp@gmail.com>
2025-07-26 23:00:15 +0200

    Make missile warning louder

---
+++ addons/BwMod_Common/CfgSounds.hpp
@@ -0,0 +1,6 @@
+class CfgSounds {
+    class BWA3_IncomingMissileWarning {
+        sound[] = {"\bwa3_common\sounds\warning_incomingMissile.wss", 5, 1}; // Make louder
+        titles[] = {};
+    };
+};
