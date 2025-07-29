8a32580f06742542415608a1784acc61f6ade05b
Timi007 <timi007gp@gmail.com>
2025-07-29 21:09:33 +0200

    Ensure BWMod keybinds are overwritten

--- addons/BwMod_Puma/initKeybinds.hpp
+++ addons/BwMod_Puma/initKeybinds.hpp
@@ -1,17 +1,22 @@
-[
-    "BWA3",
-    "BWA3_rearViewCamera",
-    LSTRING(rearViewCamera_next),
-    {[1] call FUNC(toggleCamera)},
-    "",
-    [0xC7, [true, false, false]] // Shift + Home / Pos1
-] call CBA_fnc_addKeybind;
+// Overwrite BWA3 keybind
+[{
+    !isNil {["BWA3", "BWA3_rearViewCamera"] call CBA_fnc_getKeybind}
+}, {
+    [
+        "BWA3",
+        "BWA3_rearViewCamera",
+        LSTRING(rearViewCamera_next),
+        {[1] call FUNC(toggleCamera)},
+        "",
+        [0xC7, [true, false, false]] // Shift + Home / Pos1
+    ] call CBA_fnc_addKeybind;
 
-[
-    "BWA3",
-    "BWA3_rearViewCamera_previous",
-    LSTRING(rearViewCamera_previous),
-    {[-1] call FUNC(toggleCamera)},
-    "",
-    [0xCF, [true, false, false]] // Shift + End
-] call CBA_fnc_addKeybind;
+    [
+        "BWA3",
+        "BWA3_rearViewCamera_previous",
+        LSTRING(rearViewCamera_previous),
+        {[-1] call FUNC(toggleCamera)},
+        "",
+        [0xCF, [true, false, false]] // Shift + End
+    ] call CBA_fnc_addKeybind;
+}] call CBA_fnc_waitUntilAndExecute;
