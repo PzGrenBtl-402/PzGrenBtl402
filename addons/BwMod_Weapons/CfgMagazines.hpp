9cbf6a3f66bdb8221b0702a2aee7ea408d8e9d85
Timi007 <timi007gp@gmail.com>
2025-07-29 22:27:28 +0200

    Change Puma ammo count

---
+++ addons/BwMod_Weapons/CfgMagazines.hpp
@@ -0,0 +1,15 @@
+class CfgMagazines {
+    class 140Rnd_30mm_MP_shells;
+    class BWA3_160Rnd_HE_shells: 140Rnd_30mm_MP_shells {
+        count = 240;
+    };
+
+    class BWA3_160Rnd_ABM_shells: BWA3_160Rnd_HE_shells {
+        count = 240;
+    };
+
+    class 60Rnd_30mm_APFSDS_shells;
+    class BWA3_240Rnd_APFSDS_shells: 60Rnd_30mm_APFSDS_shells {
+        count = 160;
+    };
+};
--- addons/BwMod_Weapons/config.cpp
+++ addons/BwMod_Weapons/config.cpp
@@ -15,3 +15,4 @@ class CfgPatches {
 };
 
 #include "CfgWeapons.hpp"
+#include "CfgMagazines.hpp"
