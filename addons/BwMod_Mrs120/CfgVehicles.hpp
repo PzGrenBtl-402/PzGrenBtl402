0e7b557ac708f31d0090540793757b64ede8ec1e
Timi007 <timi007gp@gmail.com>
2025-11-30 00:05:19 +0100

    Allow fire on load for BwMod Mrs120

---
+++ addons/BwMod_Mrs120/CfgVehicles.hpp
@@ -0,0 +1,8 @@
+class CfgVehicles {
+    class Mortar_01_base_F;
+    class BWA3_MRS120_base: Mortar_01_base_F {
+        class ACE_CSW {
+            allowFireOnLoad = 1;
+        };
+    };
+};
---
+++ addons/BwMod_Mrs120/script_component.hpp
@@ -0,0 +1,17 @@
+#define COMPONENT BwMod_Mrs120
+#define COMPONENT_BEAUTIFIED BwMod Mrs120
+#include "\z\PzGrenBtl402\addons\Main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define CBA_DEBUG_SYNCHRONOUS
+
+#ifdef DEBUG_ENABLED_BWMOD_MRS120
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_BWMOD_MRS120
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_BWMOD_MRS120
+#endif
+
+#include "\z\PzGrenBtl402\addons\Main\script_macros.hpp"
