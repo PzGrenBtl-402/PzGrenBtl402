0a30bc50b119edd85930ccd17571922d3e4effed
Timi007 <timi007gp@gmail.com>
2026-05-24 21:19:51 +0200

    Fix GM P2A1 not optional

--- addons/GM_P2A1/config.cpp
+++ addons/GM_P2A1/config.cpp
@@ -7,6 +7,7 @@ class CfgPatches {
         weapons[] = {};
         requiredVersion = REQUIRED_VERSION;
         requiredAddons[] = {"PzGrenBtl402_Main", "gm_weapons_pistols_p2a1"};
+        skipWhenMissingDependencies = 1;
         author = ECSTRING(Main,authors);
         authors[] = {"Timi007"};
         url = ECSTRING(Main,URL);
