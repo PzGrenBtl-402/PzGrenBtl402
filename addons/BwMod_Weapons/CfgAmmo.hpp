0364f58d025a2872d0174e8b9ff33fa8326ca872
Timi007 <timi007gp@gmail.com>
2026-08-15 17:52:41 +0200

    Increase Puma HE damage

---
+++ addons/BwMod_Weapons/CfgAmmo.hpp
@@ -0,0 +1,8 @@
+class CfgAmmo {
+    class B_30mm_MP_Tracer_Red;
+    class BWA3_B_30mm_HE: B_30mm_MP_Tracer_Red {
+        explosive = 0.95; // Previously 0.6
+        indirectHit = 9; // Previously 4
+        indirectHitRange = 3.5; // Previously 2
+    };
+};
